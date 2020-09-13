de531b502c435aea14597226a524e02dcdcd67c5
juliareich <jareich23@gmail.com>
2020-09-12 18:38:30 -0600

    completed exerPythCreateBasic

---
+++ calc.py
@@ -0,0 +1,9 @@
+# variable x and variable y
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+# Add the variable
+result = x + y
+
+# Print the solution
+print(str(x) + ' + ' + str(y) + ' = ' + str(result))
